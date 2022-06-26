766e5a0ae27d426b30e86c91522ee63e21fe75ec
devrusty <rguillory246@gmail.com>
2022-06-25 23:30:36 -0500

    fixed scripts path

--- backend/routes/scripts.js
+++ backend/routes/scripts.js
@@ -1,17 +1,18 @@
 const express = require("express")
+const pathJs = require("path")
 const fs = require("fs")
 const router = express.Router()
 
 const ROOT = { root: "../" }
 
 router.get("/:script", (req, res) => {
-    const path = `frontend/src/scripts/${req.params.script}.js`
+    const path = pathJs.resolve(process.cwd(), `frontend/src/scripts/${req.params.script}.js`)
     if (!fs.existsSync(`${ROOT.root}${path}`)) {
-        res.sendFile("/frontend/src/pages/notfound.html", ROOT)
+        res.sendFile(pathJs.resolve(process.cwd(), "frontend/src/pages/notfound.html"))
         return
     }
 
-    res.sendFile(path, ROOT)
+    res.sendFile(path)
 })
 
 module.exports = router
