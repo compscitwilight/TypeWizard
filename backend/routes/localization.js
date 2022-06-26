9fcebf43fac7fd0e6d47d6a64f5b5a4f7756ffb3
devrusty <rguillory246@gmail.com>
2022-06-25 23:38:53 -0500

    reverted

--- backend/routes/localization.js
+++ backend/routes/localization.js
@@ -7,7 +7,7 @@ const ROOT = { root: "../" }
 
 router.get("/:language", (req, res) => {
     const path = pathJs.resolve(process.cwd(), `static/localization/${req.params.language}.json`)
-    if (!fs.existsSync(path)) {
+    if (!fs.existsSync(`${ROOT.root}${path}`)) {
         res.sendFile(pathJs.resolve(process.cwd(), "/frontend/src/pages/notfound.html"))
         return
     }
