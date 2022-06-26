054de3c3e724d3b95214f449defda30582dc9175
devrusty <rguillory246@gmail.com>
2022-06-25 23:33:30 -0500

    finalised

--- backend/routes/localization.js
+++ backend/routes/localization.js
@@ -1,17 +1,18 @@
 const express = require("express")
+const pathJs = require("path")
 const fs = require("fs")
 const router = express.Router()
 
 const ROOT = { root: "../" }
 
 router.get("/:language", (req, res) => {
-    const path = `/static/localization/${req.params.language}.json`
+    const path = pathJs.resolve(process.cwd(), `static/localization/${req.params.language}.json`)
     if (!fs.existsSync(`${ROOT.root}${path}`)) {
-        res.sendFile("/frontend/src/pages/notfound.html", ROOT)
+        res.sendFile(pathJs.resolve(process.cwd(), "/frontend/src/pages/notfound.html"))
         return
     }
 
-    res.sendFile(path, ROOT)
+    res.sendFile(path)
 })
 
 module.exports = router
