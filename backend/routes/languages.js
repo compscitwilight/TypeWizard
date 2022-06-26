7dd4160ad9a44dfe2439d4f47780ee46fd07271d
devrusty <rguillory246@gmail.com>
2022-06-25 23:28:25 -0500

    fixed path issues

--- backend/routes/languages.js
+++ backend/routes/languages.js
@@ -1,11 +1,12 @@
 const express = require("express")
+const path = require("path")
 const fs = require("fs")
 const router = express.Router()
 
 const ROOT = { root: "../" }
 
 router.get("/:language", (req, res) => {
-    const path = `/static/languages/${req.params.language}.json`
+    const path = path.resolve()
     if (!fs.existsSync(`${ROOT.root}${path}`)) {
         res.sendFile("/frontend/src/pages/notfound.html", ROOT)
         return
